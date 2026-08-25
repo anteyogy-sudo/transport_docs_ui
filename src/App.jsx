fdbceb6d6d5fa982c6ca5b5ab159beb045ff62ba
Ogy_Antey <anteyogy@gmail.com>
2026-08-25 16:32:33 +0300

    first

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      
+      <div className="App">
+        <h1 className='text-3xl font-bold text-red-600'>Hello SBIS</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
